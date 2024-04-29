1bd44280774fdba2e68c253eac413b65d707b91c
Koukyosyumei <koukyosyumei@hotmail.com>
2024-04-29 17:36:57 +0900

    update

--- build/test/myplan-test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/mnt/c/Users/kanka/Desktop/Dev/MyDisassembler/build/test/myplan-test[1]_tests.cmake")
-  include("/mnt/c/Users/kanka/Desktop/Dev/MyDisassembler/build/test/myplan-test[1]_tests.cmake")
-else()
-  add_test(myplan-test_NOT_BUILT myplan-test_NOT_BUILT)
-endif()
--- build/test/myplan-test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[decoder.NOP]=]  /mnt/c/Users/kanka/Desktop/Dev/MyDisassembler/build/test/myplan-test [==[--gtest_filter=decoder.NOP]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[decoder.NOP]=]  PROPERTIES WORKING_DIRECTORY /mnt/c/Users/kanka/Desktop/Dev/MyDisassembler/build/test SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  myplan-test_TESTS decoder.NOP)
