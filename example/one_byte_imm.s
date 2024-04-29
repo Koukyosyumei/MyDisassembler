e23ca41db40257abf2171d735decf0ee5eb9e2da
Koukyosyumei <koukyosyumei@hotmail.com>
2024-04-29 19:49:38 +0900

    update

--- example/one_byte_imm.s
+++ example/one_byte_imm.s
@@ -3,6 +3,9 @@
 main:
 	mov	$0x11223344, %eax
 	mov	$0x11223344, %ecx
+	mov	$0x11223344, %r8
+	mov	$0x11223344, %r15
+    
 	add	$0x11223344, %eax
 	sub	$0x11223344, %eax
 
--- test/decoder.cpp
+++ test/decoder.cpp
@@ -18,7 +18,23 @@ TEST(decoder, ONE_BYTE) {
     state._currentIdx = 0;
     std::pair<std::string, uint64_t> res_nop = decoder.decodeSingleInstruction();
     ASSERT_EQ(res_nop.first, "NOP");
+    
+    decoder.init();
     state._currentIdx = 1;
     std::pair<std::string, uint64_t> res_ret = decoder.decodeSingleInstruction();
     ASSERT_EQ(res_ret.first, "RET");
+}
+
+TEST(decoder, ONE_BYTE_IMM) {
+    std::vector<unsigned char> obj = {0xb8, 0x44, 0x33, 0x22, 0x11};
+    DecoderState state(obj);
+    X86Decoder decoder(&state);
+
+    state._currentIdx = 0;
+    std::pair<std::string, uint64_t> res_nop = decoder.decodeSingleInstruction();
+    ASSERT_EQ(res_nop.first, "MOV");
+
+    for (auto & item : state.instructions) {
+        std::cout << item.first.first << "-" << item.first.second << ": " << item.second << std::endl;
+    }
 }
